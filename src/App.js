adb361ac64ddf6c61f97437878676df74569e9a6
Manjubarkavi622 <manjubarkavi7@gmail.com>
2020-11-17 17:56:49 +0530

    import issue

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>
+      <h1 className="header" onClick={()=> console.log("welcome")}> 
         hello world
+
       </h1>
     </div>
   );
